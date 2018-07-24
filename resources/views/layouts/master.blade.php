9af93fb10e3ca3678bd1fed5faad48df1b0214e8
christianvargasb.cr <christianvargasb.cr@gmail.com>
2018-07-24 01:42:38 -0600

    Progreso en pag home | Cambio de tema bootstrap a Litera | Cambios en master layout para separar secciones

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Ecomonedas - @yield('titulo') </title>
-        <script type="text/javascript" src="{{ URL::to('js/jquery-3.3.1.js') }}"></script>
-        <script type="text/javascript" src="{{ URL::to('js/bootstrap.min.js') }}" ></script>
-        <link rel="stylesheet" href="{{ URL::to('css/bootstrap.min.css') }}" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
-              integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+        @include('partials.head')
     </head>
-    <body>
-        @include('partials.header')
+    <body style="background-color: #e0f2f1">
+        <header>
+            @include('partials.header')
+        </header>
         <div class="container">
             @yield('contenido')
         </div>
+        <footer>
+            @include('partials.footer')
+        </footer>
     </body>
 </html>
